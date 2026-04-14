e0c4d9931c36205b811441e3b4e9b7b8bd41c361
chenkai <chenkai@bantus.cn>
2026-04-14 18:45:27 +0800

    更新

---
+++ RemiliaCode/Cards/Uncommon/RemiliaUncommon31.cs
@@ -0,0 +1,27 @@
+﻿using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using Remilia.RemiliaCode.Cards;
+using Remilia.RemiliaCode.Powers;
+
+namespace Remilia.RemiliaCode.Cards.Uncommon;
+
+public class RemiliaUncommon31() : RemiliaCard(1,
+    CardType.Power, CardRarity.Uncommon,
+    TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new PowerVar<RemiliaUncommon31Power>(25m)];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        await PowerCmd.Apply<RemiliaUncommon31Power>(base.Owner.Creature, base.DynamicVars["RemiliaUncommon31Power"].BaseValue, base.Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        base.DynamicVars["RemiliaUncommon31Power"].UpgradeValueBy(25m);
+    }
+}
---
+++ RemiliaCode/Powers/RemiliaUncommon32Power.cs
@@ -0,0 +1,43 @@
+﻿using BaseLib.Extensions;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Helpers;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.Nodes.Rooms;
+using MegaCrit.Sts2.Core.Nodes.Vfx;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace Remilia.RemiliaCode.Powers;
+
+public class RemiliaUncommon32Power : RemiliaPower
+{
+    public override PowerType Type => PowerType.Buff;
+    public override PowerStackType StackType => PowerStackType.Counter;
+
+    protected override async Task OnUse(PlayerChoiceContext choiceContext, Creature? target)
+    {
+        PotionModel.AssertValidForTargetedPotion(target);
+        await CreatureCmd.Heal(target, Math.Max((decimal)target.MaxHp * 0.3m, 1m));
+    }
+
+    public override bool ShouldDie(Creature creature)
+    {
+        if (creature != base.Owner.Creature)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public override async Task AfterPreventingDeath(Creature creature)
+    {
+        await OnUseWrapper(new ThrowingPlayerChoiceContext(), creature);
+    }
+}
