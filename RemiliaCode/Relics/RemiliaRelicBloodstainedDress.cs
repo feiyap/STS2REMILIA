4a64c8c7b34dc92d44eed64a4ee6aa7ed535f548
chenkai <chenkai@bantus.com>
2026-05-11 01:01:16 +0800

    更新

--- RemiliaCode/Relics/RemiliaRelicBloodstainedDress.cs
+++ RemiliaCode/Relics/RemiliaRelicBloodstainedDress.cs
@@ -1,14 +1,30 @@
 using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+using Remilia.RemiliaCode.Powers;
 using Remilia.RemiliaCode.Relics;
 
 namespace Remilia.RemiliaCode.Relics;
 
-[Pool(typeof(RelicPool))]
 public class RemiliaRelicBloodstainedDress() : RemiliaRelic
 {
     public override RelicRarity Rarity =>
-        RelicRarity.Starter;
-
+        RelicRarity.Uncommon;
     
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new BlockVar(1m, ValueProp.Unpowered)];
+
+    public override Task AfterPowerAmountChanged(PowerModel power, decimal amount, Creature? applier, CardModel? cardSource)
+    {
+        if (power is BloodPool && amount < 0)
+        {
+            Flash();
+            decimal count = base.DynamicVars.Block.BaseValue * -amount;
+            CreatureCmd.GainBlock(base.Owner.Creature, count, ValueProp.Unpowered, null);
+        }
+        return base.AfterPowerAmountChanged(power, amount, applier, cardSource);
+    }
 }
--- RemiliaCode/Relics/RemiliaRelicMidnightBlackTea.cs
+++ RemiliaCode/Relics/RemiliaRelicMidnightBlackTea.cs
@@ -1,14 +1,31 @@
 using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.Rooms;
+using Remilia.RemiliaCode.Powers;
 using Remilia.RemiliaCode.Relics;
 
 namespace Remilia.RemiliaCode.Relics;
 
-[Pool(typeof(RelicPool))]
 public class RemiliaRelicMidnightBlackTea() : RemiliaRelic
 {
     public override RelicRarity Rarity =>
-        RelicRarity.Starter;
-
+        RelicRarity.Common;
     
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new PowerVar<BloodPool>(2m)];
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips =>
+        [HoverTipFactory.FromPower<BloodPool>()];
+
+    public override async Task AfterRoomEntered(AbstractRoom room)
+    {
+        if (room is CombatRoom)
+        {
+            Flash();
+            await PowerCmd.Apply<BloodPool>(base.Owner.Creature, base.DynamicVars["BloodPool"].BaseValue, base.Owner.Creature, null);
+        }
+    }
 }
